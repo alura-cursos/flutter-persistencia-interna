b04e8c177192aa2f604690601f859701c79d17d1
Alex Felipe <alexfelipevieira@gmail.com>
2019-10-29 13:08:19 -0300

    default theme snippet

---
+++ theme.dart
@@ -0,0 +1,8 @@
+theme: ThemeData(
+  primaryColor: Colors.green[900],
+  accentColor: Colors.blueAccent[700],
+  buttonTheme: ButtonThemeData(
+  buttonColor: Colors.blueAccent[700],
+  textTheme: ButtonTextTheme.primary,
+  ),
+),
