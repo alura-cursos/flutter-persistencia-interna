8640a0c64baf151972698faeb4ee7c83dec41fe6
Alex Felipe <alexfelipevieira@gmail.com>
2019-10-30 08:10:58 -0300

    2.1

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Alex',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
