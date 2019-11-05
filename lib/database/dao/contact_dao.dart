5c57e03cfb1b56174f0d8b58bf8633ae96619eff
Alex Felipe <alexfelipevieira@gmail.com>
2019-11-05 12:54:15 -0300

    sample update and delete

--- lib/database/dao/contact_dao.dart
+++ lib/database/dao/contact_dao.dart
@@ -26,6 +26,26 @@ class ContactDao {
     return contacts;
   }
 
+  Future<int> update(Contact contact) async {
+    final Database db = await getDatabase();
+    final Map<String, dynamic> contactMap = _toMap(contact);
+    return db.update(
+      _tableName,
+      contactMap,
+      where: 'id = ?',
+      whereArgs: [contact.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final Database db = await getDatabase();
+    return db.delete(
+      _tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
   Map<String, dynamic> _toMap(Contact contact) {
     final Map<String, dynamic> contactMap = Map();
     contactMap[_name] = contact.name;
